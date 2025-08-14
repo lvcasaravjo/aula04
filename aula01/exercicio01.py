24028331e7a5c2841ce94b9c2ba9afa01a3de5e6
lvcasaravjo <lucas.araujo1705@gmail.com>
2025-08-14 18:54:08 -0300

    Commit segundo dia 2

---
+++ aula01/exercicio01.py
@@ -0,0 +1,8 @@
+#Crie um programa que o usuário digite seu nome e retorne o número de caracteres
+
+#minha solução
+#nome = input("Digite seu nome: ")
+#print(len(nome))
+
+#solução curso
+print(len(input("Digite seu nome: ")))
---
+++ aula01/exercicio2.py
@@ -0,0 +1,10 @@
+#Crie um programa em que o usuário digite 2 valores e retorne a soma dos mesmmos
+
+#Minha solução
+#valor1 = int(input("Digite um valor: "))
+#valor2 = int(input("Digite outro valor: "))
+#print(valor1+valor2)
+
+
+#Solução curso
+print(int(input("Digite um valor: ")) + int(input("Digite outro valor: ")))
